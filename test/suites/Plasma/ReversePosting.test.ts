3e755414e093adae58a2330b9ce415639e7fb684
zemse <sohamzemse143@gmail.com>
2020-06-08 02:36:35 +0530

    add another new proposal test case

--- test/suites/Plasma/ReversePosting.test.ts
+++ test/suites/Plasma/ReversePosting.test.ts
@@ -1,5 +1,6 @@
-import { generateBlockProposal } from './utils';
+import { ethers } from 'ethers';
 import assert from 'assert';
+import { generateBlockProposal } from './utils';
 
 // creating reversePlasmaInstanceESN with other wallet
 const _reversePlasmaInstanceESN = (walletId: number) =>
@@ -45,6 +46,31 @@ export const ReversePosting = () =>
       assert.strictEqual(validators.length, 2, 'there should now be 2 validators');
       assert.ok(
         validators.includes(global.validatorWallets[1].address),
+        'validator should be added to the array'
+      );
+    });
+
+    it('proposes new proposal expecting addition of a proposal', async () => {
+      const proposalCountBefore = await global.reversePlasmaInstanceESN.getProposalsCount(0);
+      assert.strictEqual(proposalCountBefore.toNumber(), 1, 'proposal count should be 1');
+
+      // creating a random block proposal
+      const blockProposal = ethers.utils.concat([
+        ethers.constants.HashZero,
+        ethers.utils.randomBytes(32),
+        ethers.utils.randomBytes(32),
+      ]);
+      await _reversePlasmaInstanceESN(2).proposeBlock(blockProposal, {
+        gasPrice: 0, // has zero balance initially
+      });
+
+      const proposalCountAfter = await global.reversePlasmaInstanceESN.getProposalsCount(0);
+      assert.strictEqual(proposalCountAfter.toNumber(), 2, 'proposal count should increase');
+
+      const validators = await global.reversePlasmaInstanceESN.getProposalValidators(0, 1);
+      assert.strictEqual(validators.length, 1, 'there should now be 1 validator');
+      assert.ok(
+        validators.includes(global.validatorWallets[2].address),
         'validator should be added to the array'
       );
     });
