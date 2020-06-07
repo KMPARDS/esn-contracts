e66c02dbc438165c8531b70e56de9224492a5737
zemse <sohamzemse143@gmail.com>
2020-06-07 19:33:26 +0530

    remove global defination files from cloned kami project

--- clone-kami.sh
+++ clone-kami.sh
@@ -6,4 +6,8 @@ else
     echo "Kami project does not exists, cloning it..."
     git clone https://github.com/KMPARDS/kami.git
     echo "Kami project cloned"
+    rm ./kami/src/types/global.d.ts
+    echo "Removed ./kami/src/types/global.d.ts"
+    rm ./kami/test/global.ts
+    echo "Removed ./kami/test/global.ts"
 fi
