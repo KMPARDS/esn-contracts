801ec4630b78d93b37a1f433118b1701aaf55177
zemse <sohamzemse143@gmail.com>
2020-06-08 01:48:26 +0530

    quick refactor

--- test/suites/Plasma/ReversePosting.test.ts
+++ test/suites/Plasma/ReversePosting.test.ts
@@ -10,22 +10,18 @@ const _reversePlasmaInstanceESN = (walletId: number) =>
 export const ReversePosting = () =>
   describe('Reverse Posting (of ETH blocks to ESN)', () => {
     it('proposes ETH block roots expecting addition of a proposal', async () => {
-      const blockProposal = await generateBlockProposal(0, global.providerETH);
       const proposalCountBefore = await global.reversePlasmaInstanceESN.getProposalsCount(0);
-
       assert.strictEqual(proposalCountBefore.toNumber(), 0, 'proposal count should be 0 initially');
+
+      const blockProposal = await generateBlockProposal(0, global.providerETH);
       await _reversePlasmaInstanceESN(0).proposeBlock(blockProposal, {
         gasPrice: 0, // has zero balance initially
       });
+
       const proposalCountAfter = await global.reversePlasmaInstanceESN.getProposalsCount(0);
-      assert.strictEqual(
-        proposalCountAfter.toNumber(),
-        1,
-        'proposal count should be 1 after creation'
-      );
+      assert.strictEqual(proposalCountAfter.toNumber(), 1, 'proposal count should now be 1');
 
       const validators = await global.reversePlasmaInstanceESN.getProposalValidators(0, 0);
-
       assert.ok(
         validators.includes(global.validatorWallets[0].address),
         'validator should be added to the array'
@@ -33,22 +29,18 @@ export const ReversePosting = () =>
     });
 
     it('proposes existing proposal expecting including the validator to validator array', async () => {
-      const blockProposal = await generateBlockProposal(0, global.providerETH);
       const proposalCountBefore = await global.reversePlasmaInstanceESN.getProposalsCount(0);
+      assert.strictEqual(proposalCountBefore.toNumber(), 1, 'proposal count should be 1');
 
-      assert.strictEqual(
-        proposalCountBefore.toNumber(),
-        1,
-        'proposal count should be 1 since it was created in last test case'
-      );
+      const blockProposal = await generateBlockProposal(0, global.providerETH);
       await _reversePlasmaInstanceESN(1).proposeBlock(blockProposal, {
         gasPrice: 0, // has zero balance initially
       });
+
       const proposalCountAfter = await global.reversePlasmaInstanceESN.getProposalsCount(0);
       assert.strictEqual(proposalCountAfter.toNumber(), 1, 'proposal count should be the same');
 
       const validators = await global.reversePlasmaInstanceESN.getProposalValidators(0, 0);
-
       assert.ok(
         validators.includes(global.validatorWallets[1].address),
         'validator should be added to the array'
