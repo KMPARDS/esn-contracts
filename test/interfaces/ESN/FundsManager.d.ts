d2bc3c4088234d154acaeebab44c3bdbe856944a
zemse <sohamzemse143@gmail.com>
2020-06-10 01:03:45 +0530

    Update interface of FundsManagerESN

--- test/interfaces/ESN/FundsManager.d.ts
+++ test/interfaces/ESN/FundsManager.d.ts
@@ -17,29 +17,61 @@ import {
 
 export class FundsManager extends Contract {
   functions: {
+    claimDeposit(_rawProof: BytesLike, overrides?: Overrides): Promise<ContractTransaction>;
+
+    fundsManagerETH(): Promise<{
+      0: string;
+    }>;
+
     reversePlasma(): Promise<{
       0: string;
     }>;
+
+    setFundsManagerETHAddress(
+      _fundsManagerETH: string,
+      overrides?: Overrides
+    ): Promise<ContractTransaction>;
 
     tokenOnETH(): Promise<{
       0: string;
     }>;
   };
 
+  claimDeposit(_rawProof: BytesLike, overrides?: Overrides): Promise<ContractTransaction>;
+
+  fundsManagerETH(): Promise<string>;
+
   reversePlasma(): Promise<string>;
+
+  setFundsManagerETHAddress(
+    _fundsManagerETH: string,
+    overrides?: Overrides
+  ): Promise<ContractTransaction>;
 
   tokenOnETH(): Promise<string>;
 
   filters: {};
 
   estimateGas: {
+    claimDeposit(_rawProof: BytesLike): Promise<BigNumber>;
+
+    fundsManagerETH(): Promise<BigNumber>;
+
     reversePlasma(): Promise<BigNumber>;
+
+    setFundsManagerETHAddress(_fundsManagerETH: string): Promise<BigNumber>;
 
     tokenOnETH(): Promise<BigNumber>;
   };
 
   populateTransaction: {
+    claimDeposit(_rawProof: BytesLike): Promise<PopulatedTransaction>;
+
+    fundsManagerETH(): Promise<PopulatedTransaction>;
+
     reversePlasma(): Promise<PopulatedTransaction>;
+
+    setFundsManagerETHAddress(_fundsManagerETH: string): Promise<PopulatedTransaction>;
 
     tokenOnETH(): Promise<PopulatedTransaction>;
   };
