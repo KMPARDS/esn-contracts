edfb6dd4b1143c8a5ec8a44c90430669338a07c1
zemse <sohamzemse143@gmail.com>
2020-06-09 16:06:09 +0530

    Work on Step 2 of deposits test case

--- test/suites/Plasma/Deposits.test.ts
+++ test/suites/Plasma/Deposits.test.ts
@@ -1,18 +1,50 @@
-import { ethers } from 'ethers';
+import { ethers, providers } from 'ethers';
+import { generateBlockProposal, _reversePlasmaInstanceESN } from './utils';
+import assert from 'assert';
 
 export const Deposits = () =>
   describe('Deposits (from ETH to ESN)', () => {
     it('making a deposit and getting blocks posted', async () => {
-      // transfer ES ERC20 into a fund manager contract on ETH
+      // STEP 1: transfer ES ERC20 into a fund manager contract on ETH
       await global.esInstanceETH.transfer(
         global.fundsManagerInstanceETH.address,
         ethers.utils.parseEther('10')
       );
 
-      // get the block submitted into ReversePlasma
+      // STEP 2: get the block submitted into ReversePlasma
+      const uptoblockNumber = await global.providerETH.getBlockNumber();
+      let latestBlockNumber = (
+        await global.reversePlasmaInstanceESN.latestBlockNumber()
+      ).toNumber();
 
-      // generate a merkle proof
+      await global.providerESN.send('miner_stop', []);
+      while (latestBlockNumber < uptoblockNumber) {
+        const blockProposal = await generateBlockProposal(
+          latestBlockNumber + 1,
+          global.providerETH
+        );
 
-      // submit it to the fund manager contract on ESN to get funds credited
+        await global.providerESN.send('miner_stop', []);
+        for (let i = 0; i < Math.ceil((global.validatorWallets.length * 2) / 3); i++) {
+          await _reversePlasmaInstanceESN(i).proposeBlock(blockProposal, {
+            gasPrice: 0, // has zero balance initially
+          });
+        }
+        await global.providerESN.send('miner_start', []);
+
+        await global.reversePlasmaInstanceESN.finalizeProposal(latestBlockNumber + 1, 0);
+
+        latestBlockNumber++;
+      }
+
+      let updatedBlockNumber = (
+        await global.reversePlasmaInstanceESN.latestBlockNumber()
+      ).toNumber();
+
+      assert.strictEqual(updatedBlockNumber, uptoblockNumber, 'should be same now');
+
+      // STEP 3: generate a merkle proof
+
+      // STEP 4: submit it to the fund manager contract on ESN to get funds credited
     });
   });
