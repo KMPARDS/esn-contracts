aee0dd15b0dd6a8811e0c4374fac07c337442030
zemse <sohamzemse143@gmail.com>
2020-06-10 20:03:30 +0530

    Add made up transaction test case

--- test/suites/Plasma/Deposits.test.ts
+++ test/suites/Plasma/Deposits.test.ts
@@ -1,15 +1,16 @@
 import assert from 'assert';
-import { ethers, providers } from 'ethers';
+import { ethers } from 'ethers';
 import { parseTx, getBlockFinalized, generateDepositProof } from '../../utils';
-import { ReversePlasma } from '../../interfaces/ESN';
+import { serializeTransaction } from 'ethers/lib/utils';
 
 export const Deposits = () =>
   describe('Deposits (from ETH to ESN)', () => {
+    let firstTx: ethers.ContractReceipt;
     let firstDepositProof: string;
     const amount = ethers.utils.parseEther('10');
     it('makes a deposit on ETH and gets withdrawal on ESN', async () => {
       // STEP 1: transfer ES ERC20 into a fund manager contract on ETH
-      const firstTx = await parseTx(
+      firstTx = await parseTx(
         global.esInstanceETH.transfer(global.fundsManagerInstanceETH.address, amount)
       );
 
@@ -49,6 +50,49 @@ export const Deposits = () =>
       assert.ok(
         esBalanceAfter.sub(esBalanceBefore).eq(0),
         'should not receive any ES for replayed tx'
+      );
+    });
+
+    it('tries with a made up transaction expecting revert', async () => {
+      const addr = await global.esInstanceETH.signer.getAddress();
+      const esBalanceBefore = await global.providerESN.getBalance(addr);
+
+      const tx = await global.providerETH.getTransaction(firstTx.transactionHash);
+      const madeUpProof = await generateDepositProof(firstTx.transactionHash, {
+        rawTransaction: serializeTransaction(
+          {
+            to: tx.to,
+            nonce: tx.nonce,
+            gasPrice: tx.gasPrice,
+            gasLimit: tx.gasLimit,
+            data: tx.data,
+            value: amount.add(ethers.utils.parseEther('1')),
+            chainId: tx.chainId,
+          },
+          {
+            // @ts-ignore
+            r: tx.r,
+            s: tx.s,
+            v: tx.v,
+          }
+        ),
+      });
+
+      try {
+        await parseTx(global.fundsManagerInstanceESN.claimDeposit(madeUpProof));
+        assert(false, 'should have thrown error');
+      } catch (error) {
+        const msg = error.error?.message || error.message;
+        assert.ok(
+          msg.includes('revert FM_ESN: Invalid MPT Tx proof'),
+          `Invalid error message: ${msg}`
+        );
+      }
+
+      const esBalanceAfter = await global.providerESN.getBalance(addr);
+      assert.ok(
+        esBalanceAfter.sub(esBalanceBefore).eq(0),
+        'should not receive any ES for made up tx'
       );
     });
   });
