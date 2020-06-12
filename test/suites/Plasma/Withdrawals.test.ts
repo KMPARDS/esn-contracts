ec7754f4e83f8a20786a536fe61f77b55c770b94
zemse <sohamzemse143@gmail.com>
2020-06-13 04:25:05 +0530

    Add replay proof test case (#6)

--- test/suites/Plasma/Withdrawals.test.ts
+++ test/suites/Plasma/Withdrawals.test.ts
@@ -1,10 +1,10 @@
 import assert from 'assert';
 import { ethers } from 'ethers';
-import { getBunchFinalizedFromESN } from '../../utils';
-import { generateWithdrawalProof } from '../../utils';
+import { getBunchFinalizedFromESN, parseReceipt, generateWithdrawalProof } from '../../utils';
 
 export const Withdrawals = () =>
   describe('Withdrawals (from ESN to ETH)', async () => {
+    let firstWithdrawProof: string;
     it('makes a deposit on ESN and gets withdrawal of ERC20 ES tokens on ETH', async () => {
       // STEP 1: transfer ES into the fund manager contract on ESN
       const amount = ethers.utils.parseEther('10');
@@ -20,12 +20,32 @@ export const Withdrawals = () =>
       await getBunchFinalizedFromESN(receipt.blockNumber);
 
       // STEP 3: generate a proof
-      const withdrawProof = await generateWithdrawalProof(receipt.transactionHash);
+      firstWithdrawProof = await generateWithdrawalProof(receipt.transactionHash);
 
       // STEP 4: submit proof to the fund manager contract on ETH
       const balanceBefore = await global.esInstanceETH.balanceOf(await signer.getAddress());
-      await global.fundsManagerInstanceETH.claimWithdrawal(withdrawProof);
+      await global.fundsManagerInstanceETH.claimWithdrawal(firstWithdrawProof);
       const balanceAfter = await global.esInstanceETH.balanceOf(await signer.getAddress());
       assert.ok(balanceAfter.sub(balanceBefore).eq(amount), 'should get ERC20 tokens');
+    });
+
+    it('replays the previous withdrawal expecting revert', async () => {
+      const addr = await global.providerESN.getSigner(1).getAddress();
+      const esBalanceBefore = await global.esInstanceETH.balanceOf(addr);
+      try {
+        await parseReceipt(global.fundsManagerInstanceETH.claimWithdrawal(firstWithdrawProof));
+        assert(false, 'should have thrown error');
+      } catch (error) {
+        const msg = error.error?.message || error.message;
+        assert.ok(
+          msg.includes('revert FM_ETH: Tx already claimed'),
+          `Invalid error message: ${msg}`
+        );
+      }
+      const esBalanceAfter = await global.esInstanceETH.balanceOf(addr);
+      assert.ok(
+        esBalanceAfter.sub(esBalanceBefore).eq(0),
+        'should not receive any ES for replayed tx'
+      );
     });
   });
