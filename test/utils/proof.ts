1b2d60f270062e06fc9f3fb9e5615011caf92ed9
zemse <sohamzemse143@gmail.com>
2020-06-12 20:43:06 +0530

    Organize proof util (#27)

--- test/utils/proof.ts
+++ test/utils/proof.ts
@@ -1,9 +1,113 @@
 import { ethers } from 'ethers';
 import { GetProof } from 'eth-proof';
 import { fetchBlocks } from '../../kami/src/utils/provider';
-import { getPathFromTransactionIndex } from './generateDepositProof';
 
-interface PreparingValues {
+// ---------------------------------------
+// ----------- Deposit Proof -------------
+// ---------------------------------------
+
+interface DepositProof {
+  blockNumber?: string;
+  path?: string;
+  rawTransaction?: string;
+  parentNodesTx?: string;
+  rawReceipt?: string;
+  parentNodesReceipt?: string;
+}
+
+export async function generateDepositProof(
+  txHash: string,
+  overides: DepositProof = {}
+): Promise<string> {
+  const gp = new GetProof(global.providerETH.connection.url);
+  const txProof = await gp.transactionProof(txHash);
+  const rcProof = await gp.receiptProof(txHash);
+
+  const tx = await global.providerETH.getTransaction(txHash);
+  const rawTransaction = ethers.utils.serializeTransaction(
+    {
+      to: tx.to,
+      nonce: tx.nonce,
+      gasPrice: tx.gasPrice,
+      gasLimit: tx.gasLimit,
+      data: tx.data,
+      value: tx.value,
+      chainId: tx.chainId,
+    },
+    {
+      // @ts-ignore
+      r: tx.r,
+      s: tx.s,
+      v: tx.v,
+    }
+  );
+
+  const rawReceipt = await getRawReceipt(txHash, global.providerETH);
+
+  const preparingValues: DepositProof = {
+    // @ts-ignore
+    blockNumber: overides.blockNumber || ethers.utils.hexlify(tx.blockNumber),
+    path: overides.path || getPathFromTransactionIndex(+txProof.txIndex),
+    rawTransaction: overides.rawTransaction || rawTransaction,
+    parentNodesTx: overides.parentNodesTx || ethers.utils.RLP.encode(txProof.txProof),
+    rawReceipt: overides.rawReceipt || rawReceipt,
+    parentNodesReceipt:
+      overides.parentNodesReceipt || ethers.utils.RLP.encode(rcProof.receiptProof),
+  };
+
+  const proofArray = Object.values(preparingValues);
+
+  return ethers.utils.RLP.encode(proofArray);
+}
+
+async function getRawReceipt(
+  txHash: string,
+  provider: ethers.providers.JsonRpcProvider
+): Promise<string> {
+  const receiptObj = await provider.getTransactionReceipt(txHash);
+  // @ts-ignore
+  const status = ethers.utils.hexlify(receiptObj.status ?? receiptObj.root);
+
+  return ethers.utils.RLP.encode([
+    // @ts-ignore
+    status === '0x00' ? '0x' : status,
+    receiptObj.cumulativeGasUsed.toHexString(),
+    receiptObj.logsBloom,
+    receiptObj.logs.map((log) => {
+      return [log.address, log.topics, log.data];
+    }),
+  ]);
+}
+
+export function getPathFromTransactionIndex(txIndex: number | string) {
+  let hex;
+  if (typeof txIndex === 'string') {
+    if (txIndex.slice(0, 2) === '0x') {
+      hex = txIndex.slice(2);
+    } else {
+      throw new Error('transactionIndexHex being a string should start with 0x or a number');
+    }
+  } else {
+    if (typeof txIndex === 'number') {
+      hex = txIndex.toString(16);
+    } else {
+      throw new Error('transactionIndexHex is not a number or hex string');
+    }
+  }
+
+  // @dev strippin' off zeros
+  while (hex.length && hex[0] === '0') {
+    hex = hex.slice(1);
+  }
+
+  return ethers.utils.RLP.encode('0x' + (hex.length % 2 === 0 ? hex : '0' + hex));
+}
+
+// ---------------------------------------
+// ---------- Withdraw Proof ------------
+// ---------------------------------------
+
+interface WithdrawProof {
   bunchIndex?: string;
   blockNumber?: string;
   blockInBunchMerkleProof?: string;
@@ -13,7 +117,7 @@ interface PreparingValues {
   parentNodesTx?: string;
 }
 
-export async function generateWithdrawalProof(txHash: string, overides: PreparingValues = {}) {
+export async function generateWithdrawalProof(txHash: string, overides: WithdrawProof = {}) {
   const gp = new GetProof(global.providerESN.connection.url);
   const txProof = await gp.transactionProof(txHash);
 
@@ -49,7 +153,7 @@ export async function generateWithdrawalProof(txHash: string, overides: Preparin
   const bunch = await global.plasmaManagerInstanceETH.bunches(bunchIndex);
   const block = await global.providerESN.send('eth_getBlockByNumber', [receipt.blockNumber, false]);
 
-  const preparingValues: PreparingValues = {
+  const preparingValues: WithdrawProof = {
     bunchIndex: overides.bunchIndex || ethers.utils.hexlify(bunchIndex),
     blockNumber: overides.blockNumber || ethers.utils.hexlify(receipt.blockNumber),
     blockInBunchMerkleProof:
