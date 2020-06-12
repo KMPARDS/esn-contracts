ae058951fc9be3221afd8526f4228108c2cc015c
zemse <sohamzemse143@gmail.com>
2020-06-13 04:34:08 +0530

    Add made up transaction test case (#6)

--- test/suites/Plasma/Withdrawals.test.ts
+++ test/suites/Plasma/Withdrawals.test.ts
@@ -4,19 +4,20 @@ import { getBunchFinalizedFromESN, parseReceipt, generateWithdrawalProof } from 
 
 export const Withdrawals = () =>
   describe('Withdrawals (from ESN to ETH)', async () => {
+    let firstTx: ethers.providers.TransactionResponse;
     let firstWithdrawProof: string;
+    const amount = ethers.utils.parseEther('10');
     it('makes a deposit on ESN and gets withdrawal of ERC20 ES tokens on ETH', async () => {
       // STEP 1: transfer ES into the fund manager contract on ESN
-      const amount = ethers.utils.parseEther('10');
       const signer = global.providerESN.getSigner(1);
-      const tx = await signer.sendTransaction({
+      firstTx = await signer.sendTransaction({
         to: global.fundsManagerInstanceESN.address,
         value: amount,
         gasLimit: 30000,
       });
 
       // STEP 2: getting ESN blocks posted on ETH
-      const receipt = await global.providerESN.getTransactionReceipt(tx.hash);
+      const receipt = await global.providerESN.getTransactionReceipt(firstTx.hash);
       await getBunchFinalizedFromESN(receipt.blockNumber);
 
       // STEP 3: generate a proof
@@ -46,6 +47,50 @@ export const Withdrawals = () =>
       assert.ok(
         esBalanceAfter.sub(esBalanceBefore).eq(0),
         'should not receive any ES for replayed tx'
+      );
+    });
+
+    it('tries with a made up transaction expecting revert', async () => {
+      const addr = await global.esInstanceETH.signer.getAddress();
+      const esBalanceBefore = await global.esInstanceETH.balanceOf(addr);
+
+      const madeUpProof = await generateWithdrawalProof(firstTx.hash, {
+        // creating a modifying the transaction would change the hash hence it
+        //   would be computationally difficult to prove inclusion in merkle tree
+        rawTransaction: ethers.utils.serializeTransaction(
+          {
+            to: firstTx.to,
+            nonce: firstTx.nonce,
+            gasPrice: firstTx.gasPrice,
+            gasLimit: firstTx.gasLimit,
+            data: firstTx.data,
+            value: amount.add(ethers.utils.parseEther('1')), // changing actual value
+            chainId: firstTx.chainId,
+          },
+          {
+            // @ts-ignore Need this due to r's signature (https://github.com/ethers-io/ethers.js/issues/878)
+            r: firstTx.r,
+            s: firstTx.s,
+            v: firstTx.v,
+          }
+        ),
+      });
+
+      try {
+        await parseReceipt(global.fundsManagerInstanceETH.claimWithdrawal(madeUpProof));
+        assert(false, 'should have thrown error');
+      } catch (error) {
+        const msg = error.error?.message || error.message;
+        assert.ok(
+          msg.includes('revert FM_ETH: Invalid MPT Tx proof'),
+          `Invalid error message: ${msg}`
+        );
+      }
+
+      const esBalanceAfter = await global.esInstanceETH.balanceOf(addr);
+      assert.ok(
+        esBalanceAfter.sub(esBalanceBefore).eq(0),
+        'should not receive any ES for made up tx'
       );
     });
   });
