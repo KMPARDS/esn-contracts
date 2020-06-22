f0078e4538d3a4cb64ad7b9fc7a858ae1c52567c
zemse <sohamzemse143@gmail.com>
2020-06-22 19:02:05 +0530

    Convert compile script to ts

--- compile.ts
+++ compile.ts
@@ -8,32 +8,39 @@
   npm run test (for compiling + running tests)
 */
 
-const path = require('path');
-const fs = require('fs-extra');
-const { execSync } = require('child_process');
-const solc = require('solc');
-const ethers = require('ethers');
+import { resolve } from 'path';
+import {
+  readdirSync,
+  readFileSync,
+  lstatSync,
+  existsSync,
+  removeSync,
+  ensureDirSync,
+  outputJsonSync,
+} from 'fs-extra';
+import { execSync } from 'child_process';
+import { ethers } from 'ethers';
+const solc: { compile(input: string): string } = require('solc');
 
-const filesToIgnore = { '.DS_Store': true };
-
-const sourceFolderPath = path.resolve(__dirname, 'contracts');
-const buildFolderPath = path.resolve(__dirname, 'build', 'artifacts');
-const lastSourceHashFilePath = path.resolve(__dirname, 'sst-config.json');
+const filesToIgnore: { [key: string]: boolean } = { '.DS_Store': true };
+const sourceFolderPath = resolve(__dirname, 'contracts');
+const buildFolderPath = resolve(__dirname, 'build', 'artifacts');
+const lastSourceHashFilePath = resolve(__dirname, 'sst-config.json');
 
 let sources = {};
 
-function addSourcesFromThisDirectory(relativePathArray = []) {
+function addSourcesFromThisDirectory(relativePathArray: string[] = []): void {
   const pathArray = [sourceFolderPath, ...relativePathArray];
-  fs.readdirSync(path.resolve(sourceFolderPath, ...relativePathArray)).forEach((childName) => {
+  readdirSync(resolve(sourceFolderPath, ...relativePathArray)).forEach((childName) => {
     if (filesToIgnore[childName]) return;
     const childPathArray = [...relativePathArray, childName];
-    if (fs.lstatSync(path.resolve(sourceFolderPath, ...childPathArray)).isDirectory()) {
+    if (lstatSync(resolve(sourceFolderPath, ...childPathArray)).isDirectory()) {
       addSourcesFromThisDirectory(childPathArray);
     } else {
       sources = {
         ...sources,
         [childPathArray.join('/')]: {
-          content: fs.readFileSync(path.resolve(sourceFolderPath, ...childPathArray), 'utf8'),
+          content: readFileSync(resolve(sourceFolderPath, ...childPathArray), 'utf8'),
         },
       };
     }
@@ -44,7 +51,7 @@ addSourcesFromThisDirectory();
 
 // console.log({sources});
 
-function convertToHex(inputString) {
+function convertToHex(inputString: string): string {
   var hex = '';
   for (var i = 0; i < inputString.length; i++) {
     hex += '' + inputString.charCodeAt(i).toString(16);
@@ -57,9 +64,9 @@ const sourceHash = ethers.utils.sha256('0x' + convertToHex(JSON.stringify(source
 console.log('\n'.repeat(process.stdout.rows));
 
 if (
-  fs.existsSync(buildFolderPath) &&
-  fs.existsSync(lastSourceHashFilePath) &&
-  JSON.parse(fs.readFileSync(lastSourceHashFilePath, 'utf8')).sourceHash === sourceHash
+  existsSync(buildFolderPath) &&
+  existsSync(lastSourceHashFilePath) &&
+  JSON.parse(readFileSync(lastSourceHashFilePath, 'utf8')).sourceHash === sourceHash
 ) {
   console.log('No changes in .sol files detected... \nSkiping compile script...\n');
 } else {
@@ -79,14 +86,20 @@ if (
   };
 
   console.log('Compiling contracts...');
-  const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+  interface SolcOutput {
+    errors: { severity: string; formattedMessage: string }[];
+    contracts: any[][];
+  }
+
+  const output: SolcOutput = JSON.parse(solc.compile(JSON.stringify(input)));
   console.log('Done');
 
   let shouldBuild = true;
 
   if (output.errors) {
     // console.error(output.errors);
-    for (error of output.errors) {
+    for (const error of output.errors) {
       console.log('-'.repeat(process.stdout.columns));
       console.group(error.severity.toUpperCase());
       console.log(error.formattedMessage);
@@ -94,7 +107,7 @@ if (
     }
     if (Object.values(output.errors).length) console.log('-'.repeat(process.stdout.columns));
     // throw '\nError in compilation please check the contract\n';
-    for (error of output.errors) {
+    for (const error of output.errors) {
       if (error.severity === 'error') {
         shouldBuild = false;
         throw 'Error found\n';
@@ -106,15 +119,15 @@ if (
   if (shouldBuild) {
     console.log('\nBuilding please wait...');
 
-    fs.removeSync(buildFolderPath);
-    fs.ensureDirSync(buildFolderPath);
+    removeSync(buildFolderPath);
+    ensureDirSync(buildFolderPath);
 
     let i = 0;
     for (let contractFile in output.contracts) {
       for (let key in output.contracts[contractFile]) {
         //console.log(key, Object.keys(output.contracts[contractFile][key]));
-        fs.outputJsonSync(
-          path.resolve(
+        outputJsonSync(
+          resolve(
             buildFolderPath,
             output.contracts[contractFile].length > 1
               ? `${contractFile.split('.')[0]}_${key}.json`
@@ -130,7 +143,7 @@ if (
     console.log('\nBuild failed\n');
   }
 
-  fs.outputJsonSync(path.resolve(lastSourceHashFilePath), { sourceHash });
+  outputJsonSync(resolve(lastSourceHashFilePath), { sourceHash });
 
   console.log('Running TypeChain...');
   execSync('npm run typechain');
