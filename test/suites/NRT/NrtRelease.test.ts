62f47e7db7e5739fcefb9438763be0c8dec0331e
zemse <sohamzemse143@gmail.com>
2020-06-25 20:22:36 +0530

    Add first NRT test

--- test/suites/NRT/NrtRelease.test.ts
+++ test/suites/NRT/NrtRelease.test.ts
@@ -1,5 +1,8 @@
 import assert from 'assert';
+import { ethers } from 'ethers';
 import { parseReceipt } from '../../utils';
+
+const SECONDS_IN_MONTH = 2629744;
 
 export const NrtRelease = () =>
   describe('Monthly NRT Release', () => {
@@ -13,5 +16,34 @@ export const NrtRelease = () =>
 
         assert.ok(msg.includes('NRTM: Month not finished'), `Invalid error message: ${msg}`);
       }
+    });
+
+    it('goes one month future and tries releasing NRT should success', async () => {
+      const increasedTime = await global.providerESN.send('evm_increaseTime', [SECONDS_IN_MONTH]);
+
+      const annualNRT = await global.nrtInstanceESN.annualNRT();
+      const nrtBalanceBefore = await global.providerESN.getBalance(global.nrtInstanceESN.address);
+      const timeallyBalanceBefore = await global.providerESN.getBalance(
+        global.timeallyInstance.address
+      );
+
+      await parseReceipt(global.nrtInstanceESN.releaseMonthlyNRT());
+
+      const nrtBalanceAfter = await global.providerESN.getBalance(global.nrtInstanceESN.address);
+      const timeallyBalanceAfter = await global.providerESN.getBalance(
+        global.timeallyInstance.address
+      );
+
+      assert.deepEqual(
+        nrtBalanceBefore.sub(nrtBalanceAfter),
+        annualNRT.div(12),
+        'monthly nrt should be released'
+      );
+
+      assert.deepEqual(
+        timeallyBalanceAfter.sub(timeallyBalanceBefore),
+        ethers.utils.parseEther('10237500'),
+        'timeally contract should receive NRT'
+      );
     });
   });
