6148eec8df048d751d2e1ab73a012a436ba9c4b3
zemse <sohamzemse143@gmail.com>
2020-06-26 18:42:56 +0530

    Add staking checks

--- test/suites/NRT/TimeAllyStaking.test.ts
+++ test/suites/NRT/TimeAllyStaking.test.ts
@@ -1,6 +1,24 @@
 import assert from 'assert';
 import { ethers } from 'ethers';
 import { parseReceipt, getBlockFinalizedToESN, generateDepositProof } from '../../utils';
+import { TimeAllyStakeFactory } from '../../../build/typechain/ESN';
+import { TimeAllyStake } from '../../../build/typechain/ESN/TimeAllyStake';
+
+async function getTimeAllyStakings(staker: string): Promise<TimeAllyStake[]> {
+  return (
+    await global.timeallyInstance.queryFilter(
+      global.timeallyInstance.filters.NewStaking(staker, null)
+    )
+  )
+    .map((event) => global.timeallyInstance.interface.parseLog(event))
+    .map((parsedLog) => {
+      const stakingAddress: string = parsedLog.args[1];
+      return TimeAllyStakeFactory.connect(
+        stakingAddress,
+        global.providerESN.getSigner(global.accountsESN[0])
+      );
+    });
+}
 
 export const TimeAllyStaking = () =>
   describe('TimeAlly Staking', () => {
@@ -39,6 +57,45 @@ export const TimeAllyStaking = () =>
         global.timeallyInstance.stake(0, {
           value: ethers.utils.parseEther('100'),
         })
+      );
+
+      const stakes = await getTimeAllyStakings(global.accountsESN[0]);
+      assert.strictEqual(stakes.length, 1, 'should have one staking created');
+      const stake = stakes[0];
+      assert.strictEqual(
+        await stake.nrtManager(),
+        global.nrtInstanceESN.address,
+        'nrt manager address should be set properly'
+      );
+      assert.strictEqual(
+        await stake.timeAllyManager(),
+        global.timeallyInstance.address,
+        'timeally manager address should be set properly'
+      );
+      assert.strictEqual(
+        await stake.staker(),
+        global.accountsESN[0],
+        'staker should be set correctly'
+      );
+      assert.strictEqual(
+        (await stake.stakingPlanId()).toNumber(),
+        0,
+        'plan id should be set correctly'
+      );
+      assert.strictEqual(
+        (await stake.stakingStartMonth()).toNumber(),
+        1,
+        'staking start month should be set correctly'
+      );
+      assert.strictEqual(
+        (await stake.stakingEndMonth()).toNumber(),
+        12,
+        'staking end month should be set correctly'
+      );
+      assert.strictEqual(
+        await stake.functions.unboundedBasicAmount(),
+        ethers.utils.parseEther('100').mul(2).div(100),
+        'unbounded basic amount should be set correctly'
       );
     });
   });
