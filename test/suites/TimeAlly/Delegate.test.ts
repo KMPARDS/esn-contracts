b03f5b0e269d916872bce64802930992aaef1dd9
zemse <sohamzemse143@gmail.com>
2020-06-28 20:22:58 +0530

    Add delegate tests

---
+++ test/suites/TimeAlly/Delegate.test.ts
@@ -0,0 +1,47 @@
+import { ethers } from 'ethers';
+import assert from 'assert';
+import { getTimeAllyStakings, parseReceipt } from '../../utils';
+
+export const Delegate = () =>
+  describe('Delegate', () => {
+    it('delegetes 50 ES to Validator Manager contract', async () => {
+      const stakes = await getTimeAllyStakings(global.accountsESN[0]);
+      const stake = stakes[0];
+
+      const currentMonth = await global.nrtInstanceESN.currentNrtMonth();
+      const months = [currentMonth.add(1), currentMonth.add(2)];
+      await parseReceipt(
+        stake.delegate(
+          global.validatorManagerESN.address,
+          global.validatorWallets[0].address,
+          ethers.utils.parseEther('50'),
+          months
+        )
+      );
+
+      await Promise.all(
+        months.map(async (month) => {
+          const firstDelegation = await stake.delegation(month, 0);
+          assert.strictEqual(
+            firstDelegation.platform,
+            global.validatorManagerESN.address,
+            'platform address should be set properly'
+          );
+          assert.strictEqual(
+            firstDelegation.delegatee,
+            global.validatorWallets[0].address,
+            'delegatee address should be set properly'
+          );
+          assert.deepEqual(
+            firstDelegation.amount,
+            ethers.utils.parseEther('50'),
+            'platform address should be set properly'
+          );
+
+          const monthVS = await global.validatorManagerESN.monthVS(month, 0);
+          assert.strictEqual(monthVS.validator, global.validatorWallets[0].address);
+          assert.deepEqual(monthVS.amount, ethers.utils.parseEther('50'));
+        })
+      );
+    });
+  });
--- test/suites/TimeAlly/index.ts
+++ test/suites/TimeAlly/index.ts
@@ -1,9 +1,11 @@
 import { NewStaking } from './NewStaking.test';
 import { TopupStaking } from './Topup.test';
+import { Delegate } from './Delegate.test';
 
 export const TimeAlly = () => {
   describe('TimeAlly', () => {
     NewStaking();
     TopupStaking();
+    Delegate();
   });
 };
