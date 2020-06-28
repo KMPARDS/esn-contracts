b864f485395a8a60e60cd22515d26606b512439e
zemse <sohamzemse143@gmail.com>
2020-06-28 21:28:20 +0530

    Add one staker multiple delegatee test case

--- test/suites/TimeAlly/Delegate.test.ts
+++ test/suites/TimeAlly/Delegate.test.ts
@@ -4,7 +4,7 @@ import { getTimeAllyStakings, parseReceipt } from '../../utils';
 
 export const Delegate = () =>
   describe('Delegate', () => {
-    it('delegetes 50 ES to Validator Manager contract', async () => {
+    it('delegetes 50 ES to first validator on Validator Manager contract', async () => {
       const stakes = await getTimeAllyStakings(global.accountsESN[0]);
       const stake = stakes[0];
 
@@ -40,6 +40,47 @@ export const Delegate = () =>
 
           const monthVS = await global.validatorManagerESN.monthVS(month, 0);
           assert.strictEqual(monthVS.validator, global.validatorWallets[0].address);
+          assert.deepEqual(monthVS.amount, ethers.utils.parseEther('50'));
+        })
+      );
+    });
+
+    it('delegetes 50 ES to second validator on Validator Manager contract', async () => {
+      const stakes = await getTimeAllyStakings(global.accountsESN[0]);
+      const stake = stakes[0];
+
+      const currentMonth = await global.nrtInstanceESN.currentNrtMonth();
+      const months = [currentMonth.add(1), currentMonth.add(2)];
+      await parseReceipt(
+        stake.delegate(
+          global.validatorManagerESN.address,
+          global.validatorWallets[1].address,
+          ethers.utils.parseEther('50'),
+          months
+        )
+      );
+
+      await Promise.all(
+        months.map(async (month) => {
+          const secondDelegation = await stake.delegation(month, 1);
+          assert.strictEqual(
+            secondDelegation.platform,
+            global.validatorManagerESN.address,
+            'platform address should be set properly'
+          );
+          assert.strictEqual(
+            secondDelegation.delegatee,
+            global.validatorWallets[1].address,
+            'delegatee address should be set properly'
+          );
+          assert.deepEqual(
+            secondDelegation.amount,
+            ethers.utils.parseEther('50'),
+            'platform address should be set properly'
+          );
+
+          const monthVS = await global.validatorManagerESN.monthVS(month, 1);
+          assert.strictEqual(monthVS.validator, global.validatorWallets[1].address);
           assert.deepEqual(monthVS.amount, ethers.utils.parseEther('50'));
         })
       );
