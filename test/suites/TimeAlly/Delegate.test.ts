a52482a74216fb6d8bb8601556184fa3b57ac5b7
zemse <sohamzemse143@gmail.com>
2020-06-29 21:24:48 +0530

    Add same validator delegated by other staking

--- test/suites/TimeAlly/Delegate.test.ts
+++ test/suites/TimeAlly/Delegate.test.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers';
 import assert from 'assert';
 import { getTimeAllyStakings, parseReceipt } from '../../utils';
+import { TimeAllyStakeFactory } from '../../../build/typechain/ESN';
 
 export const Delegate = () =>
   describe('Delegate', () => {
@@ -108,5 +109,61 @@ export const Delegate = () =>
 
         assert.ok(msg.includes('TAStake: delegate overflow'), `Invalid error message: ${msg}`);
       }
+    });
+
+    it('delegates 100 ES with other staking to first validator on Validator Manager contract', async () => {
+      await global.providerESN.getSigner(global.accountsESN[0]).sendTransaction({
+        to: global.accountsESN[1],
+        value: ethers.utils.parseEther('100'),
+      });
+
+      const _timeallyInstanceESN = global.timeallyInstanceESN.connect(
+        global.providerESN.getSigner(global.accountsESN[1])
+      );
+
+      const r = await parseReceipt(
+        _timeallyInstanceESN.stake(0, {
+          value: ethers.utils.parseEther('100'),
+        })
+      );
+      const newStakingEvent = global.timeallyInstanceESN.interface.parseLog(r.logs[0]);
+      const stakingContractAddress: string = newStakingEvent.args.staking;
+
+      const stake = TimeAllyStakeFactory.connect(
+        stakingContractAddress,
+        global.providerESN.getSigner(global.accountsESN[1])
+      );
+
+      const currentMonth = await global.nrtInstanceESN.currentNrtMonth();
+      const month = currentMonth.add(1);
+      await parseReceipt(
+        stake.delegate(
+          global.validatorManagerESN.address,
+          global.validatorWallets[0].address,
+          ethers.utils.parseEther('100'),
+          [month]
+        )
+      );
+
+      const firstDelegation = await stake.delegation(month, 0);
+      assert.strictEqual(
+        firstDelegation.platform,
+        global.validatorManagerESN.address,
+        'platform address should be set properly'
+      );
+      assert.strictEqual(
+        firstDelegation.delegatee,
+        global.validatorWallets[0].address,
+        'delegatee address should be set properly'
+      );
+      assert.deepEqual(
+        firstDelegation.amount,
+        ethers.utils.parseEther('100'),
+        'platform address should be set properly'
+      );
+
+      const monthVS = await global.validatorManagerESN.monthVS(month, 0);
+      assert.strictEqual(monthVS.validator, global.validatorWallets[0].address);
+      assert.deepEqual(monthVS.amount, ethers.utils.parseEther('150'));
     });
   });
