47f6115599d78d9336e28228557091f759072468
zemse <sohamzemse143@gmail.com>
2020-08-30 04:05:40 +0530

    Update test cases

--- test/suites/KycDapp/UpdateKycStatus.test.ts
+++ test/suites/KycDapp/UpdateKycStatus.test.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers';
 import { parseEther, formatBytes32String } from 'ethers/lib/utils';
 import { parseReceipt } from '../../utils';
+import { strictEqual } from 'assert';
 
 let wallet = ethers.Wallet.createRandom();
 const username = 'gocart12';
@@ -9,6 +10,14 @@ export const UpdateKycStatus = () =>
   describe('Update Kyc Status', () => {
     before(async () => {
       wallet = wallet.connect(global.providerESN);
+
+      try {
+        await global.dayswappersInstanceESN.getSeatByAddress(global.accountsESN[0]);
+      } catch {
+        await global.dayswappersInstanceESN.join(ethers.constants.AddressZero);
+      }
+
+      await global.dayswappersInstanceESN.connect(wallet).join(global.accountsESN[0]);
 
       await global.providerESN.getSigner(0).sendTransaction({
         to: wallet.address,
@@ -20,7 +29,7 @@ export const UpdateKycStatus = () =>
       });
     });
 
-    it('marks Kyc level 1 as resolved', async () => {
+    it('updates Kyc level 1 as resolved', async () => {
       await parseReceipt(
         global.kycDappInstanceESN.updateKycStatus(
           formatBytes32String(username),
@@ -28,9 +37,10 @@ export const UpdateKycStatus = () =>
           ethers.constants.AddressZero,
           ethers.constants.HashZero,
           1
-        ),
-        true,
-        true
+        )
       );
+
+      const kycStatus = await global.kycDappInstanceESN.isKycLevel1(wallet.address);
+      strictEqual(kycStatus, true, 'kyc should now be approved');
     });
   });
