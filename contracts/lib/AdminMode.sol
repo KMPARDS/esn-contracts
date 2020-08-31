f3444cf86ece3d98abec07bfc7fdca67248e5845
zemse <sohamzemse143@gmail.com>
2020-09-01 03:19:39 +0530

    Add view method to admin mode

--- contracts/lib/AdminMode.sol
+++ contracts/lib/AdminMode.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.7.0;
 import { ERC173 } from "./ERC173.sol";
 
 contract WithAdminMode is ERC173 {
-    bool public adminMode = true;
+    bool private adminMode = true;
 
     modifier whenAdminMode() {
         require(adminMode, "AdminMode: ADMIN_MODE_INACTIVE");
@@ -14,5 +14,9 @@ contract WithAdminMode is ERC173 {
 
     function renounceMigrationMode() public whenAdminMode onlyOwner {
         adminMode = false;
+    }
+
+    function isAdminMode() public view returns (bool) {
+        return adminMode;
     }
 }
