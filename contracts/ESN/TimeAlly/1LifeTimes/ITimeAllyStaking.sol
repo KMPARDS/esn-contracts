66c4f9850fb7fd13f61e470f3ca9c2a934a196b9
zemse <sohamzemse143@gmail.com>
2020-09-07 23:57:28 +0530

    Add basic staking interface

---
+++ contracts/ESN/TimeAlly/1LifeTimes/ITimeAllyStaking.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+interface ITimeAllyStaking {
+    function startMonth() external view returns (uint32);
+
+    function principal() external view returns (uint256);
+}
