a8ed984365bb2582ce75e8339e2c9198672fd041
zemse <sohamzemse143@gmail.com>
2020-09-24 03:58:03 +0530

    Fix ds migration test case

--- test/suites/Dayswappers/Migration.test.ts
+++ test/suites/Dayswappers/Migration.test.ts
@@ -3,27 +3,27 @@ import { parseReceipt } from '../../utils';
 import { strictEqual } from 'assert';
 import { formatBytes32String, parseEther } from 'ethers/lib/utils';
 
-const migrationData: {
-  owner: string;
-  kycResolved: boolean;
-  incompleteKycResolveSeatIndex: ethers.BigNumberish;
-  depth: ethers.BigNumberish;
-  introducer: string;
-  beltIndex: ethers.BigNumberish;
-}[] = [
-  {
-    owner: ethers.Wallet.createRandom().address,
-    kycResolved: true,
-    incompleteKycResolveSeatIndex: 0,
-    depth: 3,
-    introducer: ethers.Wallet.createRandom().address,
-    beltIndex: 4,
-  },
-];
-
 export const Migration = () =>
   describe('Migration', () => {
     it('migrates few accounts', async () => {
-      await parseReceipt(global.dayswappersInstanceESN.migrateSeats(migrationData), true);
+      const migrationData: {
+        owner: string;
+        kycResolved: boolean;
+        incompleteKycResolveSeatIndex: ethers.BigNumberish;
+        depth: ethers.BigNumberish;
+        introducer: string;
+        beltIndex: ethers.BigNumberish;
+      }[] = [
+        {
+          owner: ethers.Wallet.createRandom().address,
+          kycResolved: true,
+          incompleteKycResolveSeatIndex: 0,
+          depth: 3,
+          introducer: global.accountsESN[0],
+          beltIndex: 4,
+        },
+      ];
+
+      await parseReceipt(global.dayswappersInstanceESN.migrateSeats(migrationData));
     });
   });
