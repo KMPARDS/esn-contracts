a04cb566585c45eec4d9fce9189710e2e6c265a7
zemse <sohamzemse143@gmail.com>
2020-09-30 21:13:00 +0530

    Add BetDeEx contract

---
+++ contracts/ESN/BetDeEx/Bet.sol
@@ -0,0 +1,251 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { BetDeEx } from "./BetDeEx.sol";
+
+// import { BetInterface } from "./BetInterface.sol";
+import { RegistryDependent } from "../KycDapp/RegistryDependent.sol";
+import { Governable } from "../Governance/Governable.sol";
+
+/// @title Bet Smart Contract
+/// @author The EraSwap Team
+/// @notice This contract governs bettors and is deployed by BetDeEx Smart Contract
+
+contract Bet is Governable, RegistryDependent {
+    using SafeMath for uint256;
+
+    BetDeEx betDeEx;
+
+    string public description; /// @dev question text of the bet
+    bool public isDrawPossible; /// @dev if false then user cannot bet on draw choice
+    uint8 public category; /// @dev sports, movies
+    uint8 public subCategory; /// @dev cricket, football
+    uint256 public upVotes;
+    uint256 public downVotes;
+    // address private _owner; replaced with Openzeppelin's ownable
+
+    uint8 public finalResult; /// @dev given a value when manager ends bet
+    address public endedBy; /// @dev address of manager who enters the correct choice while ending the bet
+
+    //uint256 public creationTimestamp; /// @dev set during bet creation
+    uint256 public pauseTimestamp; /// @dev set as an argument by deployer
+    uint256 public endTimestamp; /// @dev set when a manager ends bet and prizes are distributed
+
+    uint256 public minimumBetInExaEs; /// @dev minimum amount required to enter bet
+    uint256 public prizePercentPerThousand; /// @dev percentage of bet balance which will be dristributed to winners and rest is platform fee
+    uint256[3] public totalBetTokensInExaEsByChoice = [0, 0, 0]; /// @dev array of total bet value of no, yes, draw voters
+    uint256[3] public getNumberOfChoiceBettors = [0, 0, 0]; /// @dev stores number of bettors in a choice
+
+    uint256 public totalPrize; /// @dev this is the prize (platform fee is already excluded)
+
+    mapping(address => uint256) public betBalanceInExaEs;
+    mapping(address => uint256[3]) public bettorBetAmountInExaEsByChoice; /// @dev mapps addresses to array of betAmount by choice
+    mapping(address => bool) public bettorHasClaimed; /// @dev set to true when bettor claims the prize
+
+    event NewBetting(
+        address indexed _betAddress,
+        address indexed _bettorAddress,
+        uint8 indexed _choice,
+        uint256 _betTokensInExaEs
+    );
+
+    event EndBetContract(
+        address indexed _ender,
+        address indexed _contractAddress,
+        uint8 _result,
+        uint256 _platformFee
+    );
+
+    event TransferES(address indexed _to, uint256 _tokensInExaEs);
+
+    /* /// @notice this is an internal functionality that is only for bet contracts to emit a event when a new bet is placed so that front end can get the information by subscribing to  contract
+    function emitNewBettingEvent(address _bettorAddress, uint8 _choice, uint256 _betTokensInExaEs) public onlyBetContract {
+        emit NewBetting(msg.sender, _bettorAddress, _choice, _betTokensInExaEs);
+    }
+    /// @notice this is an internal functionality that is only for bet contracts to emit event when a bet is ended so that front end can get the information by subscribing to  contract
+    function emitEndEvent(address _ender, uint8 _result, uint256 _gasFee) public onlyBetContract {
+        emit EndBetContract(_ender, msg.sender, _result, _gasFee);
+    }*/
+
+    /*modifier onlyManager() override{
+        require(betDeEx.isManager(msg.sender), "only manager can call");
+        _;
+    }*/
+
+    modifier onlyKycApproved {
+        require(kycDapp().isKycLevel1(msg.sender), "KYC needs to be completed");
+        _;
+    }
+
+    function renounceOwnership() public virtual override(Governable) pure {
+        revert("ERC173: RENOUNCE_NOT_ALLOWED");
+    }
+
+    /// @notice this sets up Bet contract
+    /// @param _description is the question of Bet in plain English, bettors have to understand the bet description and later choose to bet on yes no or draw according to their preference
+    /// @param _category is the broad category for example Sports. Purpose of this is only to filter bets and show in UI, hence the name of the category is not stored in smart contract and category is repressented by a number (0, 1, 2, 3...)
+    /// @param _subCategory is a specific category for example Football. Each category will have sub categories represented by a number (0, 1, 2, 3...)
+    /// @param _minimumBetInExaEs is the least amount of ExaES that can be betted, any bet participant (bettor) will have to bet this amount or higher. Betting higher amount gives more share of winning amount
+    /// @param _prizePercentPerThousand is a form of representation of platform fee. It is a number less than or equal to 1000. For eg 2% is to be collected as platform fee then this value would be 980. If 0.2% then 998.
+    /// @param _isDrawPossible is functionality for allowing a draw option
+    /// @param _pauseTimestamp Bet will be open for betting until this timestamp, after this timestamp, any user will not be able to place bet. and manager can only end bet after this time
+
+    function initialize(
+        address _owner1,
+        string memory _description,
+        uint8 _category,
+        uint8 _subCategory,
+        uint256 _minimumBetInExaEs,
+        uint256 _prizePercentPerThousand,
+        bool _isDrawPossible,
+        uint256 _pauseTimestamp,
+        address _kycDapp
+    ) public {
+        // _owner = _owner1;s
+        transferOwnership(_owner1);
+        description = _description;
+        isDrawPossible = _isDrawPossible;
+        category = _category;
+        subCategory = _subCategory;
+        minimumBetInExaEs = _minimumBetInExaEs;
+        prizePercentPerThousand = _prizePercentPerThousand;
+        betDeEx = BetDeEx(msg.sender);
+        //creationTimestamp = now;
+        pauseTimestamp = _pauseTimestamp;
+        upVotes = 0;
+        downVotes = 0;
+        setKycDapp(_kycDapp);
+    }
+
+    /// @notice this function gives amount of ExaEs that is total betted on this bet
+    //Remove
+    function totalContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    /// @notice this function is used to place a bet on available choice
+    /// @param _choice should be 0, 1, 2; no => 0, yes => 1, draw => 2
+    /// @param _betTokensInExaEs is amount of bet
+    function enterBet(uint8 _choice, uint256 _betTokensInExaEs) public payable onlyKycApproved {
+        require(block.timestamp < pauseTimestamp, "Cannot enter after pause time");
+        require(
+            _betTokensInExaEs >= minimumBetInExaEs,
+            "Betting tokens should be more than minimum"
+        );
+
+        /// @dev betDeEx contract transfers the tokens to it self
+        //require(betDeEx.collectBettorTokens(msg.sender, _betTokensInExaEs), "betting tokens should be collected");
+        require(msg.value == _betTokensInExaEs, "Tokens not received as the exact entered amount");
+        betBalanceInExaEs[msg.sender] = betBalanceInExaEs[msg.sender].add(_betTokensInExaEs);
+
+        // @dev _choice can be 0 or 1 and it can be 2 only if isDrawPossible is true
+        if (_choice > 2 || (_choice == 2 && !isDrawPossible)) {
+            require(false, "This choice is not available");
+        }
+
+        getNumberOfChoiceBettors[_choice] = getNumberOfChoiceBettors[_choice].add(1);
+        totalBetTokensInExaEsByChoice[_choice] = totalBetTokensInExaEsByChoice[_choice].add(
+            _betTokensInExaEs
+        );
+
+        bettorBetAmountInExaEsByChoice[msg.sender][_choice] = bettorBetAmountInExaEsByChoice[msg
+            .sender][_choice]
+            .add(_betTokensInExaEs);
+
+        uint256 _treeAmount = _betTokensInExaEs.mul(4).div(1000); // 0.4% given to the introducer tree
+        uint256 _introducerAmount = _betTokensInExaEs.mul(4).div(1000); // 0.4% given to the introducer
+        betDeEx.payRewards(_treeAmount, _introducerAmount);
+
+        betDeEx.emitNewBettingEvent(msg.sender, _choice, _betTokensInExaEs);
+    }
+
+    /// @notice this function is used by manager to load correct answer
+    /// @param _choice is the correct choice
+    function endBet(uint8 _choice) public {
+        //require(now >= pauseTimestamp, "cannot end bet before pause time");
+        require(endedBy == address(0), "Bet: ALREADY_ENDED");
+
+        // @dev _choice can be 0 or 1 and it can be 2 only if isDrawPossible is true
+        if (_choice < 2 || (_choice == 2 && isDrawPossible)) {
+            finalResult = _choice;
+        } else {
+            require(false, "Bet: CHOICE_NOT_AVAILABLE");
+        }
+
+        endedBy = msg.sender;
+        endTimestamp = block.timestamp;
+
+        /// @dev the platform fee is excluded from entire bet balance and this is the amount to be distributed
+        totalPrize = totalContractBalance().mul(prizePercentPerThousand).div(1000);
+
+        // @dev this is the left platform fee according to the totalPrize variable above
+        uint256 _platformFee = totalContractBalance().sub(totalPrize);
+
+        /// @dev sending plaftrom fee to the super manager
+        //require(betDeEx.collectPlatformFee(_platformFee), "platform fee should be collected");
+
+        betDeEx.emitEndBetEvent(endedBy, _choice, _platformFee);
+    }
+
+    function upvote() public {
+        ++upVotes;
+    }
+
+    function downvote() public {
+        ++downVotes;
+    }
+
+    /// @notice this function can be called by anyone to see how much winners are getting
+    /// @param _bettorAddress is address whose prize we want to see
+    /// @return winner prize of input address
+    function seeWinnerPrize(address _bettorAddress) public view returns (uint256) {
+        require(endTimestamp > 0, "Bet: CANNOT_SEE_PRIZE_BEFORE_BET_ENDS");
+
+        return
+            bettorBetAmountInExaEsByChoice[_bettorAddress][finalResult].mul(totalPrize).div(
+                totalBetTokensInExaEsByChoice[finalResult]
+            );
+    }
+
+    /// @notice this function will be called after bet ends and winner bettors can withdraw their prize share
+    function withdrawPrize(address payable _bettorAddress) public payable onlyKycApproved {
+        require(endTimestamp > 0, "Bet: CANNOT_WITHDRAW_BEFORE_END_TIME");
+        require(!bettorHasClaimed[msg.sender], "Bet: CANNOT_CLAIM_AGAIN");
+        require(_bettorAddress == msg.sender, "Bet: ONLY_BETTOR_CAN_CLAIM_HIS_WINNINGS");
+        require(
+            bettorBetAmountInExaEsByChoice[msg.sender][finalResult] >= minimumBetInExaEs,
+            "Bet: CALLER_SHOULD_HAVE_A_BETTING"
+        ); // @dev to keep out option 0
+        uint256 _winningAmount = bettorBetAmountInExaEsByChoice[msg.sender][finalResult]
+            .mul(totalPrize)
+            .div(totalBetTokensInExaEsByChoice[finalResult]);
+        bettorHasClaimed[msg.sender] = true;
+        betBalanceInExaEs[msg.sender] = betBalanceInExaEs[msg.sender].sub(_winningAmount);
+        //msg.value == _winningAmount;
+        payable(_bettorAddress).transfer(_winningAmount);
+        emit TransferES(_bettorAddress, _winningAmount);
+        /*betDeEx.sendTokensToAddress(
+            msg.sender,
+            _winningAmount
+        );*/
+    }
+
+    /*yswappers dayswappersContract;
+    
+    /// @dev sending amount to dayswappers
+    function payMoney() public {
+        address _userAddress; // @dev address of user to who's tree we want to send ES
+        uint256 _txAmount = 1 ether; // @dev on ESN, ether is ES
+        uint256 _treeAmount = _txAmount.mul(40).div(100); //0.4% of Total 
+        uint256 _introducerAmount = _txAmount.mul(40).div(100); // 0.5% of txAmount
+        
+        /// @dev sending value to a payable function along with giving an argument to the method
+        dayswappersContract.payToTree{value: _treeAmount}(_userAddress, [uint256(50), uint256(0), uint256(50)]);
+        dayswappersContract.payToIntroducer{value: _introducerAmount}(_userAddress, [uint256(50), uint256(0), uint256(50)]);
+        
+        /// @dev report volume generated. useful for user to attain a active status.
+        dayswappersContract.reportVolume(_userAddress, _txAmount);
+    }*/
+}
---
+++ contracts/ESN/BetDeEx/BetDeEx.sol
@@ -0,0 +1,162 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import { EIP1167CloneFactory } from "../../lib/EIP1167CloneFactory.sol";
+import { Governable } from "../Governance/Governable.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { Bet } from "./Bet.sol";
+// import { BetInterface } from "./BetInterface.sol";
+import { IKycDapp } from "../KycDapp/IKycDapp.sol";
+import { IDayswappers } from "../Dayswappers/IDayswappers.sol";
+import { RegistryDependent } from "../KycDapp/RegistryDependent.sol";
+
+/// @title BetDeEx Smart Contract - The Decentralized Prediction Platform of Era Swap Ecosystem
+/// @author The EraSwap Team
+/// @notice This contract is used by manager to deploy new Bet contracts
+/// @dev This contract also acts as treasurer
+
+contract BetDeEx is EIP1167CloneFactory, Governable, RegistryDependent {
+    using SafeMath for uint256;
+    //IKycDapp KycDappContract;
+
+    address public implementation; /// @dev stores the address of implementation contract
+    address[] public clonedContracts; /// @dev returns the address of cloned contracts
+    //address public owner; /// @dev Required to be public because ES needs to be sent transaparently.
+    // address private _owner;
+
+    mapping(address => bool) public isBetValid; /// @dev Stores authentic bet contracts (only deployed through this contract)
+    // Remove betBalanceInExaEs
+    //mapping(address => uint256) public betBalanceInExaEs; /// @dev All ES tokens are transfered to main BetDeEx address for common allowance in ERC20 so this mapping stores total ES tokens betted in each bet.
+    /*mapping(address => bool) public isBetValid; /// @dev Stores authentic bet contracts (only deployed through this contract)
+    mapping(address => bool) public KYC; /// @dev Checks whether a particular user has a KYC or not*/
+
+    event NewBetEvent(
+        address indexed _deployer, //Show the bets of deployer
+        address _contractAddress,
+        uint8 indexed _category,
+        uint8 indexed _subCategory,
+        string _description
+    );
+
+    event NewBetting(
+        address indexed _betAddress,
+        address indexed _bettorAddress,
+        uint8 indexed _choice,
+        uint256 _betTokensInExaEs
+    );
+
+    event EndBetContract(
+        address indexed _ender,
+        address indexed _contractAddress,
+        uint8 _result,
+        uint256 _platformFee
+    );
+
+    /// @dev This event is for indexing ES withdrawl transactions to winner bettors from this contract
+
+    //Add owner
+    /*modifier onlyOwner() {
+        require(msg.sender == owner, "only superManager can call");
+        _;
+    }*/
+
+    /* modifier onlyBetContract() {
+        require(isBetValid[msg.sender], "Only bet contract can call");
+        _;
+    }
+    */
+    modifier onlyKycApproved {
+        require(kycDapp().isKycLevel1(msg.sender), "BetDeEx: KYC_REQUIRED");
+        _;
+    }
+
+    modifier onlyBetContract {
+        require(isBetValid[msg.sender], "BetDeEx: ONLY_BET_CAN_CALL");
+        _;
+    }
+
+    /// @notice Sets up BetDeEx smart contract when deployed
+
+    constructor() {
+        // _owner = msg.sender; // gets done in by governable
+    }
+
+    /*function setKYC(address user) public{
+        KYC[user] = true;
+    }*/
+
+    //Add onlyowner
+    function storageFactory(address _implementation) public onlyGovernance {
+        implementation = _implementation;
+    }
+
+    function createBet(
+        string memory _description,
+        uint8 _category,
+        uint8 _subCategory,
+        uint256 _minimumBetInExaEs,
+        uint256 _prizePercentPerThousand,
+        bool _isDrawPossible,
+        uint256 _pauseTimestamp
+    ) public onlyKycApproved {
+        // Add KYC check
+        // Add upvote and downvote
+        // Add a feature to sort by volume
+        address clone = createClone(implementation);
+        clonedContracts.push(clone);
+        Bet(clone).initialize(
+            owner(),
+            _description,
+            _category,
+            _subCategory,
+            _minimumBetInExaEs,
+            _prizePercentPerThousand,
+            _isDrawPossible,
+            _pauseTimestamp,
+            address(kycDapp())
+        );
+        isBetValid[address(clone)] = true;
+        emit NewBetEvent(msg.sender, address(clone), _category, _subCategory, _description);
+    }
+
+    /// @notice this function is used for getting total number of bets
+    /// @return number of Bet contracts deployed by BetDeEx
+    function getNumberOfBets() public view returns (uint256) {
+        return clonedContracts.length;
+    }
+
+    /// @notice this is an internal functionality that is only for bet contracts to emit a event when a new bet is placed so that front end can get the information by subscribing to  contract
+    function emitNewBettingEvent(
+        address _bettorAddress,
+        uint8 _choice,
+        uint256 _betTokensInExaEs
+    ) public onlyBetContract {
+        emit NewBetting(msg.sender, _bettorAddress, _choice, _betTokensInExaEs);
+    }
+
+    /// @notice this is an internal functionality that is only for bet contracts to emit event when a bet is ended so that front end can get the information by subscribing to  contract
+    function emitEndBetEvent(
+        address _ender,
+        uint8 _result,
+        uint256 _gasFee
+    ) public onlyBetContract {
+        emit EndBetContract(_ender, msg.sender, _result, _gasFee);
+    }
+
+    function payRewards(uint256 _treeAmount, uint256 _introducerAmount)
+        public
+        payable
+        onlyBetContract
+    {
+        IDayswappers _dayswappersContract = IDayswappers(resolveAddress("DAYSWAPPERS"));
+
+        _dayswappersContract.payToTree{ value: _treeAmount }(
+            msg.sender,
+            [uint256(50), uint256(0), uint256(50)]
+        );
+        _dayswappersContract.payToIntroducer{ value: _introducerAmount }(
+            msg.sender,
+            [uint256(50), uint256(0), uint256(50)]
+        );
+    }
+}
