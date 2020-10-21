297320e254c74362be296fd719e8e4b8cbfa7c9c
zemse <sohamzemse143@gmail.com>
2020-10-21 20:32:46 +0530

    Add NRTReceived event

--- contracts/ESN/NRT/NRTReceiver.sol
+++ contracts/ESN/NRT/NRTReceiver.sol
@@ -12,12 +12,16 @@ abstract contract NRTReceiver is INRTReceiver, RegistryDependent {
     /// @dev Maps NRT month to NRT amount received in the month.
     mapping(uint32 => uint256) monthlyNRT;
 
+    event NRTReceived(uint32 indexed month, uint256 indexed amount, address sender);
+
     /// @notice Allows NRT Manager contract to send NRT share for TimeAlly.
     function receiveNrt(uint32 _currentNrtMonth) public virtual override payable {
         address payable _nrtManager = payable(resolveAddress("NRT_MANAGER"));
         require(msg.sender == _nrtManager, "NRTReceiver: Only NRT can send");
         // uint256 currentNrtMonth = NRTManager(_nrtManager).currentNrtMonth();
         monthlyNRT[_currentNrtMonth] = msg.value;
+
+        emit NRTReceived(_currentNrtMonth, msg.value, msg.sender);
     }
 
     function getMonthlyNRT(uint32 _month) public virtual override view returns (uint256) {
