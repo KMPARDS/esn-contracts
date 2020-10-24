7f12e085d51ce0c9b09189bf5ad475316cf5b4cb
zemse <sohamzemse143@gmail.com>
2020-10-24 14:22:34 +0530

    Add addresses and mainnet switch

--- scripts/commons.ts
+++ scripts/commons.ts
@@ -40,67 +40,60 @@ interface ExistingContractAddresses {
 // ATTENTION: Ensure NRT SECONDS_IN_MONTH is 0 for testnet
 // testnet chain
 export const existing: ExistingContractAddresses = {
-  // esEth: '',
-  // plasmaEth: '',
-  // fundsManEth: '',
-  // rplasmaEsn: '',
-  // fundsManEsn: '',
-  // proxyAdmin: '',
-  // nrtManager: '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
-  // timeallyManager: '0x7F87f9830baB8A591E6f94fd1A47EE87560B0bB0',
-  // timeallyStakingTarget: '0xA3C6cf908EeeebF61da6e0e885687Cab557b5e3F',
-  // validatorSet: '0x8418249278d74D46014683A8029Fd6fbC88482a1',
-  // validatorManager: '0xE14D14bd8D0E2c36f5E4D00106417d8cf1000e22',
-  // randomnessManager: '0x44F70d80642998F6ABc424ceAf1E706a479De8Ce',
-  // blockRewardManager: '0x2AA786Cd8544c50136e5097D5E19F6AE10E02543',
-  // prepaidEs: '0x22E0940C1AE5D31B9efBaf7D674F7D62895FBde8',
-  // dayswappers: '0xC677B02C7B63aD664cBf54EAe6fA3C759D072742',
-  // kycdapp: '0xC4336494606203e3907539d5b462A5cb7853B3C6',
-  // timeallyclub: '0x6D57FaDF31e62E28Ab059f3dCd565df055428c57',
-  // timeAllyPromotionalBucket: '0xaDbA96fDA88B0Cbcf11d668FF6f7A29d062eD050',
-  // betdeex: '0x8Cef02AB53256439dBFC83C7845786b6E60af989',
-  // betImplementation: '0x27F13c6e064b892D0a36548487329Eb19fC164A2',
-  // timeswappers: '0xC5E48826651017e1d8D113119B10Abb094D919B8', // dont comment
-  // buzcafe: '0x35f97f35ba126677B3183B3b6a29f46CA8BC18sE3', // dont comment
-  // powertoken: '0xC94A1C28a0c10D52A9a7A1756693F9B92672d15C', // dont comment
-  // buildSurvey: '0xCf535dB3c1EDbFbBdadbDe725119906BE20fb362',
-  // rentingDappManager: '0x5854C0813b5692C8e0F232B1f84aF37f20E3571b',
-  // tsgap: '0x98dD383CE722eFc881354cE38922d50017C3eE89',
-  // petLiquid: '',
-  // petPrepaid: '',
+  esEth: '0x237027559f6C07A20EBa97C837b60b9815840a42',
+  plasmaEth: '0xaaF33029B457A773C14DFdab4eDc4039E80fC5BF',
+  fundsManEth: '0x10A23bbfAfc7cc3b94fa268D965F8B78543eCFE0',
+  rplasmaEsn: '0x3bEb087e33eC0B830325991A32E3F8bb16A51317',
+  fundsManEsn: '0xc4cfb05119Ea1F59fb5a8F949288801491D00110',
+  proxyAdmin: '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
+  nrtManager: '0x8418249278d74D46014683A8029Fd6fbC88482a1',
+  timeallyManager: '0x2AA786Cd8544c50136e5097D5E19F6AE10E02543',
+  timeallyStakingTarget: '0x22E0940C1AE5D31B9efBaf7D674F7D62895FBde8',
+  validatorSet: '0xF9FCb8678dB15A5507A5f5414D68aBB2f4568E27',
+  validatorManager: '0xaDbA96fDA88B0Cbcf11d668FF6f7A29d062eD050',
+  randomnessManager: '0xCf535dB3c1EDbFbBdadbDe725119906BE20fb362',
+  blockRewardManager: '0x7DD7EDB18C271959156967bc7651D685E8C1B225',
+  prepaidEs: '0x56578Ff4c37Fd4e02C8d75FF9982A22C095dD3c5',
+  dayswappers: '0xee42b2Dcc3d32AD5E736df6245AD8A88a70ba6bF',
+  kycdapp: '0x8b2C9732137bAD7e629139B1fDa9E6094368f6B4',
+  timeallyclub: '0x9e805A912edf6Ce7A57790f2797835Ff6220E5b0',
+  timeAllyPromotionalBucket: '0x99660616e8922a1887E1683A6836f2cf916F4B2a',
+  betdeex: '0x238FA401068d4b4Ba186Da30e84023AA1a763d17',
+  betImplementation: '0x3aE4071a068De6f00a34ACE0Aec43CAc8cb87077',
+  buildSurvey: '0x99c691E9592255673AB5CB3a2497B25ef77206d3',
+  rentingDappManager: '0x73861A6C82C9342E30744353216D1f597642AD71',
+  tsgap: '0xf6cA67cC19435A64a8D9911cF39Dc39dB63Ae1c8',
+  petLiquid: '0x427D4946eE290A49Ac215D1fC64e465C457D99De',
+  petPrepaid: '0x527778e73eC371979F85826C50EF8758d60366F0',
 };
 // local
 // export const existing: ExistingContractAddresses = {
-// nrtManager: '0x3bEb087e33eC0B830325991A32E3F8bb16A51317',
-// timeallyManager: '0xc4cfb05119Ea1F59fb5a8F949288801491D00110',
-// timeallyStakingTarget: '0x961D3860d840D6ACCeAA302fbF5C0bb83b801bb4',
-// validatorSet: '0x7eCb2899D9D66858D558391D448e6e4D1B4a86cF',
-// validatorManager: '0xcA4d0578c5e07F0964C7E7ccc87E606A234625b8',
-// randomnessManager: '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
-// blockRewardManager: '0x7F87f9830baB8A591E6f94fd1A47EE87560B0bB0',
-// prepaidEs: '0xA3C6cf908EeeebF61da6e0e885687Cab557b5e3F',
-// dayswappers: '0x8418249278d74D46014683A8029Fd6fbC88482a1',
-// kycdapp: '0xE14D14bd8D0E2c36f5E4D00106417d8cf1000e22',
-// timeallyclub: '0x44F70d80642998F6ABc424ceAf1E706a479De8Ce',
-// timeAllyPromotionalBucket: '0x2AA786Cd8544c50136e5097D5E19F6AE10E02543',
-// tsgap: '0xC85dE468d545eD44a986b31D1c5d604733FB4A33',
+//   nrtManager: '0x961D3860d840D6ACCeAA302fbF5C0bb83b801bb4',
+//   timeallyManager: '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
+//   timeallyStakingTarget: '0x961D3860d840D6ACCeAA302fbF5C0bb83b801bb4',
+//   validatorSet: '0x7eCb2899D9D66858D558391D448e6e4D1B4a86cF',
+//   validatorManager: '0xcA4d0578c5e07F0964C7E7ccc87E606A234625b8',
+//   randomnessManager: '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
+//   blockRewardManager: '0x7F87f9830baB8A591E6f94fd1A47EE87560B0bB0',
+//   prepaidEs: '0xCf535dB3c1EDbFbBdadbDe725119906BE20fb362',
+//   dayswappers: '0x8418249278d74D46014683A8029Fd6fbC88482a1',
+//   kycdapp: '0x56578Ff4c37Fd4e02C8d75FF9982A22C095dD3c5',
+//   timeallyclub: '0x44F70d80642998F6ABc424ceAf1E706a479De8Ce',
+//   timeAllyPromotionalBucket: '0x2AA786Cd8544c50136e5097D5E19F6AE10E02543',
+//   tsgap: '0xC85dE468d545eD44a986b31D1c5d604733FB4A33',
 // };
 
+// const MAINNET = true;
+const MAINNET = false;
+
 export const providerETH = new ethers.providers.InfuraProvider(
-  'rinkeby',
+  MAINNET ? 'homestead' : 'rinkeby',
   'b20d36f9ea564d3a9b3ac01578948856'
 );
 
-const network = {
-  name: 'EraSwapNetwork',
-  chainId: 5196,
-  ensAddress: '0xC4336494606203e3907539d5b462A5cb7853B3C6',
-};
-
 export const providerESN = new CustomProvider(
-  'https://node2.testnet.eraswap.network',
-  // 'http://localhost:8545',
-  network
+  MAINNET ? 'https://mainnet.eraswap.network' : 'https://testnet.eraswap.network'
+  // 'http://localhost:8545'
 );
 
 let wallet: ethers.Wallet | null = null;
