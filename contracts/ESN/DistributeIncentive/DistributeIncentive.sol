b45c71501aa6aff748f1968f8698c1c6d7865fa2
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2020-12-27 09:00:23 +0530

    Distribute Incentive

--- contracts/ESN/DistributeIncentive/DistributeIncentive.sol
+++ contracts/ESN/DistributeIncentive/DistributeIncentive.sol
@@ -91,7 +91,6 @@ contract DistributeIncentive is Governable, RegistryDependent {
         (bool _successCharity, ) = charity.call{ value: _reward.mul(10).div(100) }("");
         require(_successCharity, "Incentive: CHARITY_TRANSFER_IS_FAILING");
 
-
         dayswappers().reportVolume(_buyer, _value);
 
         emit SendIncentive(_seller, _buyer, _value);
